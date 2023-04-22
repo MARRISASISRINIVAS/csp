e9bec931535fca46d24789d999b0ea31ccbb107b
MARRISASISRINIVAS <93903566+MARRISASISRINIVAS@users.noreply.github.com>
2023-04-23 02:48:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -49,7 +49,7 @@ async def start(client, message):
         await db.add_user(message.from_user.id, message.from_user.first_name)
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
-        buttons =elif query.data == "mystery":
+        buttons =elif query.data == "start":
         buttons = [[
                     InlineKeyboardButton('Mystery', callback_data='mystery'),
                     InlineKeyboardButton('Horror', callback_data='horror')
